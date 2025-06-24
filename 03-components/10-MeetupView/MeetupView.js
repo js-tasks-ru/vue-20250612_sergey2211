840e609b41f8b31208ce3a7ffce2833c387d0a84
Mindoza <serg-214-sas@mail.ru>
2025-06-24 13:33:48 +0500

    Solve MeetupView

--- 03-components/10-MeetupView/MeetupView.js
+++ 03-components/10-MeetupView/MeetupView.js
@@ -10,32 +10,56 @@ export default defineComponent({
   name: 'MeetupView',
 
   components: {
+    MeetupInfo,
+    MeetupAgenda,
+    MeetupDescription,
+    MeetupCover,
     UiAlert,
     UiContainer,
   },
 
+  props: {
+    meetup: {
+      type: Object,
+      required: true
+    },
+  },
+
   template: `
     <div>
-
-      <!-- Обложка митапа -->
+      <MeetupCover
+        :title="meetup.title"
+        :image="meetup.image"
+      />
 
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
             <h2>Описание</h2>
 
-            <!-- Описание митапа -->
+            <MeetupDescription
+              :description="meetup.description"
+            />
 
             <h2>Программа</h2>
 
-            <!-- Программа митапа -->
-            <!-- Или при пустой программе - сообщение "Программа пока пуста..." в UiAlert -->
-            <UiAlert></UiAlert>
+            <MeetupAgenda
+              v-if="meetup.agenda.length"
+              :agenda="meetup.agenda"
+            />
+            <UiAlert
+              v-else
+              text="Программа пока пуста..."
+            />
 
           </div>
           <div class="meetup__aside">
 
-            <!-- Краткая информация о митапе -->
+            <MeetupInfo
+              :organizer="meetup.organizer"
+              :place="meetup.place"
+              :date="meetup.date"
+            />
 
             <div class="meetup__aside-buttons"></div>
           </div>
