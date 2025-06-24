4436a1d9cb0db73852210c048fcd50cf91ddd240
Mindoza <serg-214-sas@mail.ru>
2025-06-24 13:53:24 +0500

    Solve UiClock

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,28 @@
-import { defineComponent } from 'vue'
+import {computed, defineComponent, onMounted, onUnmounted, ref} from 'vue'
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
+    const currentTime = ref(Date.now());
+    const tick = ref();
 
-  template: `<div class="clock">10:12:02</div>`,
+    onMounted(() => {
+      tick.value = setInterval(() => {
+        currentTime.value = Date.now()
+      }, 1000)
+    })
+
+    onUnmounted(() => {
+      clearInterval(tick)
+    })
+
+    const time = computed(() => {
+      return new Date(currentTime.value).toLocaleTimeString('en-US', { timeStyle: 'medium' })
+    })
+
+    return { time }
+  },
+
+  template: `<div class="clock"> {{ time }} </div>`,
 })
