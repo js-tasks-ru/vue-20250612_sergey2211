76e6a723242c575d9f960fa6a871d84001e29177
Mindoza <serg-214-sas@mail.ru>
2025-06-24 08:12:32 +0500

    Solve Counter

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,26 +1,41 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const inputNumber = ref('0');
+    const min = 0;
+    const max = 5;
+
+    return {
+      inputNumber,
+      min,
+      max,
+    }
+  },
 
   template: `
     <div class="counter">
       <button
         class="button button--secondary"
+        :disabled="inputNumber<=min"
         type="button"
         aria-label="Decrement"
-        disabled
-      >➖</button>
+        @click="inputNumber--"
+      >➖
+      </button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count"> {{ inputNumber }} </span>
 
       <button
         class="button button--secondary"
+        :disabled="inputNumber>=max"
         type="button"
         aria-label="Increment"
-      >➕</button>
+        @click="inputNumber++"
+      >➕
+      </button>
     </div>
   `,
 })
